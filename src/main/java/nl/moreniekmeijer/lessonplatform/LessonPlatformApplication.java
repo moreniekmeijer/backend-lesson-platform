16ea266f80202c9addf6bad26b89a57b1f9ec7a1
moreniekmeijer <niekjmeijer@gmail.com>
2025-02-16 16:50:53 +0100

    First commit, made basic file structure

---
+++ src/main/java/nl/moreniekmeijer/lessonplatform/LessonPlatformApplication.java
@@ -0,0 +1,13 @@
+package nl.moreniekmeijer.lessonplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/moreniekmeijer/lessonplatform/LessonPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.moreniekmeijer.lessonplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
