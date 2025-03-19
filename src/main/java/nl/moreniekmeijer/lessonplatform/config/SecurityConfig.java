59420dbf124a0dfdbf4c4323dca950e1c901629e
moreniekmeijer <niekjmeijer@gmail.com>
2025-03-19 21:53:11 +0100

    Added basic SecurityConfig

---
+++ src/main/java/nl/moreniekmeijer/lessonplatform/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package nl.moreniekmeijer.lessonplatform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorization -> authorization
+                        .requestMatchers(HttpMethod.GET).authenticated()
+                        .requestMatchers("/**").hasRole("ADMIN")
+                        .anyRequest().denyAll()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
+
