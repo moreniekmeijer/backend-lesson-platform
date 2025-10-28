507c684acc5d9533ba323762e0c62a6fa48abb39
moreniekmeijer <niekjmeijer@gmail.com>
2025-10-28 18:20:25 +0100

    Adjusted CORS for frontend

--- src/main/java/nl/moreniekmeijer/lessonplatform/config/CORSConfig.java
+++ src/main/java/nl/moreniekmeijer/lessonplatform/config/CORSConfig.java
@@ -15,8 +15,8 @@ public class CORSConfig
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                        .allowedOrigins("http://localhost:5173", "https://frontend-lesson-platform.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("*");
             }
