b452caf223fe91d3c72e7a29a879e2777bb0cf41
moreniekmeijer <niekjmeijer@gmail.com>
2025-11-03 17:12:57 +0100

    Updated CORS

--- src/main/java/nl/moreniekmeijer/lessonplatform/config/CORSConfig.java
+++ src/main/java/nl/moreniekmeijer/lessonplatform/config/CORSConfig.java
@@ -15,7 +15,7 @@ public class CORSConfig
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://frontend-lesson-platform.netlify.app")
+                        .allowedOrigins("http://localhost:5173", "https://frontend-lesson-platform.netlify.app", "https://percussieplatform.moreniekmeijer.nl")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("*");
