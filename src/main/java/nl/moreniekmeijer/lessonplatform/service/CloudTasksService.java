2b941730ec39b7903cd8d8ded39235e25acfc46b
moreniekmeijer <niekjmeijer@gmail.com>
2025-11-06 14:00:19 +0100

    Added OIDC-token to CloudTasksService

--- src/main/java/nl/moreniekmeijer/lessonplatform/service/CloudTasksService.java
+++ src/main/java/nl/moreniekmeijer/lessonplatform/service/CloudTasksService.java
@@ -1,10 +1,6 @@
 package nl.moreniekmeijer.lessonplatform.service;
 
-import com.google.cloud.tasks.v2.CloudTasksClient;
-import com.google.cloud.tasks.v2.HttpMethod;
-import com.google.cloud.tasks.v2.HttpRequest;
-import com.google.cloud.tasks.v2.QueueName;
-import com.google.cloud.tasks.v2.Task;
+import com.google.cloud.tasks.v2.*;
 import com.google.protobuf.ByteString;
 import org.springframework.stereotype.Service;
 
@@ -15,23 +11,33 @@ public class CloudTasksService {
     private final String location = "europe-west1";
     private final String queue = "video-conversion-queue";
     private final String targetUrl = "https://video-converter-service-2-250364193521.europe-west1.run.app/convert";
+    private final String serviceAccountEmail = "service-250364193521@gcp-sa-cloudtasks.iam.gserviceaccount.com";
 
     public void enqueueVideoConversion(Long materialId, String objectName) {
         System.out.println("Creating task for materialId=" + materialId + ", objectName=" + objectName);
 
         try (CloudTasksClient client = CloudTasksClient.create()) {
-            Task.Builder taskBuilder = Task.newBuilder()
-                    .setHttpRequest(HttpRequest.newBuilder()
-                            .setHttpMethod(HttpMethod.POST)
-                            .setUrl(targetUrl)
-                            .setBody(ByteString.copyFromUtf8(
-                                    String.format("{\"materialId\":%d,\"objectName\":\"%s\"}", materialId, objectName)
-                            ))
-                            .putHeaders("Content-Type", "application/json")
-                    );
+            // Maak de HTTP-request en voeg OIDC-token toe
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .setHttpMethod(HttpMethod.POST)
+                    .setUrl(targetUrl)
+                    .setBody(ByteString.copyFromUtf8(
+                            String.format("{\"materialId\":%d,\"objectName\":\"%s\"}", materialId, objectName)
+                    ))
+                    .putHeaders("Content-Type", "application/json")
+                    .setOidcToken(OidcToken.newBuilder()
+                            .setServiceAccountEmail(serviceAccountEmail)
+                            .build())
+                    .build();
+
+            Task task = Task.newBuilder()
+                    .setHttpRequest(httpRequest)
+                    .build();
 
             QueueName parent = QueueName.of(projectId, location, queue);
-            client.createTask(parent, taskBuilder.build());
+            client.createTask(parent, task);
+
+            System.out.println("Task created successfully!");
 
         } catch (Exception e) {
             e.printStackTrace();
